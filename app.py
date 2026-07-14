d97a76437d9fe8acc4f60e25bdd2226487573bc6
Naman Sharma <namangd456@gmail.com>
2026-07-14 17:24:33 +0530

    Updated app in master_1

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import json
 
 app = Flask(__name__)
 
-MONGO_URI = "mongodb+srv://Namstar:Naman1234@flask-dev.7d8kgcd.mongodb.net/?appName=flask-dev"
-client = MongoClient(MONGO_URI)
-db = client['flask-dev']
-collection = db['Submissions']
+# MONGO_URI = "mongodb+srv://Namstar:Naman1234@flask-dev.7d8kgcd.mongodb.net/?appName=flask-dev"
+# client = MongoClient(MONGO_URI)
+# db = client['flask-dev']
+# collection = db['Submissions']
 
 @app.route('/api')
 def api():
@@ -24,19 +24,20 @@ def index():
             email = request.form.get("email")
             message = request.form.get("message")
 
-            collection.insert_one({
-                "name": name,
-                "email": email,
-                "message": message
-            })
-            return redirect(url_for('success'))
+            print(name)
+            print(email)
+            print(message)
+
+            return redirect(url_for("success"))
+
         except Exception as e:
             error = str(e)
-    return render_template("index.html", error=error)
 
+    return render_template("index.html", error=error)
 @app.route("/success")
 def success():
     return render_template("success.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
